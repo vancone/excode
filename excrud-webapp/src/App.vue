5150843e9023b8a1fc8ebf08eeaeb91fe1be3892
Tenton Lien <tentonlien@users.noreply.github.com>
2020-06-22 14:11:41 +0800

    Rebuild web app

--- excrud-webapp/src/App.vue
+++ excrud-webapp/src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
-    <router-view />
+    <div class="top-bar">
+      <h1 class="app-title">ExCRUD</h1>
+    </div>
+    <router-view/>
   </div>
 </template>
 
@@ -9,3 +12,32 @@ export default {
   name: 'App'
 }
 </script>
+
+<style>
+html,body {
+  margin: 0;
+  height: 100%;
+  width: 100%;
+  background-color: #f7f7f7;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+.top-bar {
+  background-color: white;
+  height: 50px;
+  width: 100%;
+  text-align: left;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.25);
+}
+.app-title {
+  margin: 0;
+  font-size: 20px;
+  line-height: 50px;
+  margin-left: 20px;
+}
+</style>
---
+++ excrud-webapp/src/components/Initial.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="hello">
+    <el-row :gutter="20">
+      <el-col :span="4">
+        <div>&nbsp;</div>
+      </el-col>
+      <el-col :span="16">
+        <h2 class="projects-title">Projects</h2>
+        <el-table :data="tableData" class="project-table">
+          <el-table-column prop="date" label="Project name" ></el-table-column>
+          <el-table-column prop="name" label="Creator" width="200"></el-table-column>
+          <el-table-column prop="address" label="Modified time" width="200"></el-table-column>
+        </el-table>
+      </el-col>
+      <el-col :span="4">
+        <div style="text-align: left">
+          <button class="button-create">New project</button>
+        </div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App",
+      tableData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "13:55, Jun. 22, 2020"
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "13:55, Jun. 22, 2020"
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "13:55, Jun. 22, 2020"
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "13:55, Jun. 22, 2020"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.projects-title {
+  text-align: left;
+  font-size: 18px;
+  margin-top: 40px;
+  margin-left: 20px;
+  margin-bottom: 20px;
+
+}
+.project-table {
+  margin-left: 20px;
+  width: calc(100% - 40px);
+  background: none;
+}
+.button-create {
+  background-color: #195f71;
+  color: white;
+  border-radius: 2px;
+  height: 30px;
+  width: 150px;
+  outline: none;
+  border: none;
+  margin-top: 40px;
+  cursor: pointer;
+}
+
+.el-table--fit{
+        /* padding: 20px; */
+}
+.el-table, .el-table__expanded-cell {
+    background-color: transparent;
+}
+
+.el-table tr {
+    background-color: transparent!important;
+}
+.el-table--enable-row-transition .el-table__body td, .el-table .cell{
+    background-color: transparent;
+}
+</style>
